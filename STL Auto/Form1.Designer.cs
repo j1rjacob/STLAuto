86f1322c86f5013b3dd18d691db7ecd75f00ad36
j1rjacob <junarjacob@yahoo.com>
2018-03-24 18:27:15 +0300

    updates

--- STL Auto/Form1.Designer.cs
+++ STL Auto/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace STL_Auto
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.OpenFileDialogExcel = new System.Windows.Forms.OpenFileDialog();
+            this.ButtonCompute = new System.Windows.Forms.Button();
+            this.TextBoxFiles = new System.Windows.Forms.TextBox();
+            this.ButtonBrowse = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OpenFileDialogExcel
+            // 
+            this.OpenFileDialogExcel.FileName = "openFileDialog1";
+            // 
+            // ButtonCompute
+            // 
+            this.ButtonCompute.Location = new System.Drawing.Point(456, 24);
+            this.ButtonCompute.Name = "ButtonCompute";
+            this.ButtonCompute.Size = new System.Drawing.Size(75, 23);
+            this.ButtonCompute.TabIndex = 0;
+            this.ButtonCompute.Text = "Compute";
+            this.ButtonCompute.UseVisualStyleBackColor = true;
+            // 
+            // TextBoxFiles
+            // 
+            this.TextBoxFiles.Location = new System.Drawing.Point(32, 24);
+            this.TextBoxFiles.Name = "TextBoxFiles";
+            this.TextBoxFiles.Size = new System.Drawing.Size(224, 20);
+            this.TextBoxFiles.TabIndex = 1;
+            // 
+            // ButtonBrowse
+            // 
+            this.ButtonBrowse.Location = new System.Drawing.Point(288, 24);
+            this.ButtonBrowse.Name = "ButtonBrowse";
+            this.ButtonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.ButtonBrowse.TabIndex = 2;
+            this.ButtonBrowse.Text = "Browse";
+            this.ButtonBrowse.UseVisualStyleBackColor = true;
+            this.ButtonBrowse.Click += new System.EventHandler(this.ButtonBrowse_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(288, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(384, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Write";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(603, 335);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ButtonBrowse);
+            this.Controls.Add(this.TextBoxFiles);
+            this.Controls.Add(this.ButtonCompute);
+            this.Name = "FormMain";
+            this.Text = "Excel Auto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog OpenFileDialogExcel;
+        private System.Windows.Forms.Button ButtonCompute;
+        private System.Windows.Forms.TextBox TextBoxFiles;
+        private System.Windows.Forms.Button ButtonBrowse;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- STL Auto/Form1.cs
+++ STL Auto/Form1.cs
@@ -1,20 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ExcelLibrary.SpreadSheet;
+using OfficeOpenXml;
+using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace STL_Auto
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void ButtonBrowse_Click(object sender, EventArgs e)
+        {
+
+            //try
+            //{
+            //    if (openFileDialogMeter.ShowDialog() == DialogResult.OK)
+            //    {
+            //        ImportMeters(openFileDialogMeter.FileNames);
+            //    }
+            //    MessageBox.Show("Import was successful");
+            //}
+            //catch (Exception)
+            //{
+            //    MessageBox.Show("Import was not successful");
+            //}
+            
+            var fileinfo = new FileInfo(@"E:\MaskRider\FormsTemplate\Gosi.xlsx");
+            if (fileinfo.Exists)
+            {
+                using (ExcelPackage excelPackage = new ExcelPackage(fileinfo))
+                {
+                    //ExcelWorkbook excelWorkBook = excelPackage.Workbook;
+                    ExcelWorksheet excelWorksheet = excelPackage.Workbook.Worksheets[1];
+                    excelWorksheet.Cells["O142"].Value = "Test";
+                    //excelWorksheet.Cells[3, 2].Value = "Test2";
+                    //excelWorksheet.Cells[3, 3].Value = "Test3";
+
+                    excelPackage.Save();
+                    Console.WriteLine("Done");
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //create new xls file 
+            string file = "C:\newdoc.xls";
+            Workbook workbook = new Workbook();
+            Worksheet worksheet = new Worksheet("First Sheet");
+            worksheet.Cells[0, 1] = new Cell((short)1); worksheet.Cells[2, 0] = new Cell(9999999);
+            worksheet.Cells[3, 3] = new Cell((decimal)3.45);
+            worksheet.Cells[2, 2] = new Cell("Text string");
+            worksheet.Cells[2, 4] = new Cell("Second string");
+            worksheet.Cells[4, 0] = new Cell(32764.5, "#,##0.00");
+            worksheet.Cells[5, 1] = new Cell(DateTime.Now, @"YYYY-MM-DD");
+            worksheet.Cells.ColumnWidth[0, 1] = 3000; workbook.Worksheets.Add(worksheet);
+            workbook.Save(file);
+
+            // open xls file 
+            Workbook book = Workbook.Load(file);
+            Worksheet sheet = book.Worksheets[0];
+
+            // traverse cells 
+            //foreach (Pair<,>, Cell> cell in sheet.Cells)
+            //{
+            //    dgvCells[cell.Left.Right, cell.Left.Left].Value = cell.Right.Value;
+            //}
+
+            // traverse rows by Index 
+            for (int rowIndex = sheet.Cells.FirstRowIndex; rowIndex <= sheet.Cells.LastRowIndex; rowIndex++)
+            {
+                Row row = sheet.Cells.GetRow(rowIndex);
+                for (int colIndex = row.FirstColIndex; colIndex <= row.LastColIndex; colIndex++)
+                {
+                    Cell cell = row.GetCell(colIndex);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string file = @"E:\MaskRider\Book1.xls";
+            //Stream fileStream = System.IO.File.OpenRead(file);
+            Workbook book = Workbook.Load(file);
+            Worksheet sheet = book.Worksheets[0];
+            sheet.Cells[25, 14] = new Cell("Test from junar");
+            book.Save(file);
         }
     }
 }
